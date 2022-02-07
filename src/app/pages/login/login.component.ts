9455d8a21804bff926f274720e0f232ceccc61d9
Donatus Brain Okwe <braindona@gmail.com>
2022-02-07 04:32:08 -0600

    Some changes on login and Register component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -12,4 +12,27 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // To be implemented in the authService. // Just a test
+  // setlocalStorage(responseObj: any) {
+  //   const expires = moment().add(responseObj.token);
+
+  //   localStorage.setItem('token', responseObj.token);
+  //   localStorage.setItem('expires', JSON.stringify(expires.valueOf()));
+  // }
+
+  // logout() {
+  //   localStorage.removeItem('token');
+  //   localStorage.removeItem('expires');
+  // }
+
+  // getExpiration() {
+  //   const expiration = localStorage.getItem('expires')!;
+  //   const expiresAt = JSON.parse(expiration);
+  //   return moment(expiresAt);
+  // }
+
+  // isLoggedIn() {
+  //   return moment().isBefore(this.getExpiration());
+  // }
+
 }
